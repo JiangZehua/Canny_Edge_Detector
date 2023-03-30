4dcc21be18bb90314e6057605b215838e5c6b57e
Zehua Jiang <zj2086@nyu.edu>
2023-03-30 14:47:23 -0400

    init commit

---
+++ CannyEdgeDetect.py
@@ -0,0 +1,15 @@
+'''
+Canny Edge Detection
+'''
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+
+
+if __name__ == "__main__":
+    image1 = "/Users/zehuajiang/Desktop/Project_1_Canny_Edge_Detector/Barbara.bmp"
+    image2 = "/Users/zehuajiang/Desktop/Project_1_Canny_Edge_Detector/Goldhill.bmp"
+    image3 = "/Users/zehuajiang/Desktop/Project_1_Canny_Edge_Detector/Peppers.bmp"
+    
